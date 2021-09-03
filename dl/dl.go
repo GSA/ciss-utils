1e738c03f1746f28cfe8503dd130ac36397f6f9e
wei zhu <zhu_wei@bah.com>
2021-09-03 11:37:59 -0400

    Remove duplicate func

--- dl/dl.go
+++ dl/dl.go
@@ -3,7 +3,6 @@ package dl
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -11,6 +10,7 @@ import (
 	"strings"
 )
 
+// Requires gh cli tool installed
 func CreateReleasePath(outfile string) (string, error) {
 	wd, err := os.Getwd()
 	if err != nil {
@@ -79,29 +79,4 @@ func DownloadGHRelease(target string, tag string) error {
 	return Run(map[string][]string{
 		"gh": {"release", "download", "-D" + filepath.Dir(path), "-R" + ownerRepo, "-p" + filename, tag},
 	})
-}
-
-func save(r io.Reader, path string) error {
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		return fmt.Errorf("failed to open %s: %v", path, err)
-	}
-	defer func() {
-		err := f.Close()
-		if err != nil {
-			fmt.Printf("failed to close %s file handle: %v", path, err)
-		}
-	}()
-	_, err = io.Copy(f, r)
-	if err != nil {
-		return fmt.Errorf("failed to save inventory: %v", err)
-	}
-	return nil
-}
-
-func FileExists(path string) bool {
-	_, err := os.Stat(path)
-
-	// return the negative of is not exist
-	return !os.IsNotExist(err)
 }
